4e338001b8cad9e7ce9f9fa72826e2616bf88163
avallete <avallete@e1r5p3.42.fr>
2015-01-11 11:30:33 +0100

    modify lib, add ft_filler.c

--- libft/ft_filler.c
+++ libft/ft_filler.c
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_converter_int.c                                 :+:      :+:    :+:   */
+/*   ft_filler.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: avallete <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/10 16:30:50 by avallete          #+#    #+#             */
-/*   Updated: 2015/01/11 11:25:01 by avallete         ###   ########.fr       */
+/*   Created: 2015/01/11 11:25:50 by avallete          #+#    #+#             */
+/*   Updated: 2015/01/11 11:28:17 by avallete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <ft_printf.h>
+#include <libft.h>
 
-void	print_int(va_list list, int *i)
+void	ft_filler(char c, size_t len)
 {
-	int c;
+	size_t i;
 
-	c = va_arg(list, int);
-	ft_putnbr(c);
-	i[1] += ft_nbrlen(c);
-}
-
-void	arg_is_int(t_flags *flags, va_list list, int *i)
-{
-	if ((flags->type == 'd' || flags->type == 'i') && flags->formf == 'l')
-		flags = flags;
-	else
-		print_int(list, i);
+	i = 0;
+	while (i++ <= len)
+		ft_putchar(c);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: avallete <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/10 16:29:05 by avallete          #+#    #+#             */
-/*   Updated: 2015/01/11 11:08:26 by avallete         ###   ########.fr       */
+/*   Updated: 2015/01/11 11:22:58 by avallete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int main(void)
 	int ret1;
 	int ret2;
 
-	ret1 = printf("pjolie test %%%%%s%c%c\n", "un foutu Test\n", 'l', 'w');
-	ret2 = ft_printf("fjolie test %%%%%s%c%c\n", "un foutu Test\n", 'l', 'w');
+	ret1 = printf("pjolie test %%%%%s%c%c%d%i\n", "un foutu Test\n", 'l', 'w', 100, 50);
+	ret2 = ft_printf("fjolie test %%%%%s%c%c%d%i\n", "un foutu Test\n", 'l', 'w', 100, 50);
 	ft_putnbr(ret1);
 	ft_putnbr(ret2);
 	return (0);

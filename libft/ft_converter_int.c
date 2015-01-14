e2214123a7723ccb83b1cb012ba2e8fb32c2c724
avallete <avallete@e1r4p3.42.fr>
2015-01-14 18:59:00 +0100

    test 52 2 dots rest

--- libft/ft_converter_int.c
+++ libft/ft_converter_int.c
@@ -6,7 +6,7 @@
 /*   By: avallete <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/10 16:30:50 by avallete          #+#    #+#             */
-/*   Updated: 2015/01/14 17:10:36 by avallete         ###   ########.fr       */
+/*   Updated: 2015/01/14 18:58:39 by avallete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,52 @@
 
 void	fill_it(t_flags *flags, int size)
 {
-	if (flags->optzero && !(flags->prec))
-		ft_filler('0', size);
-	else
-		ft_filler(' ', size);
+  if (flags->optzero && !(flags->prec))
+    ft_filler('0', size);
+  else
+    ft_filler(' ', size);
 }
 
 void	print_plus(t_flags *flags, int *i, int c)
 {
-		if (flags->optplus && c > 0)
-		{
-			ft_putchar('+');
-			i[1] += 1;
-		}
+  if (flags->optplus && c > 0)
+  {
+    ft_putchar('+');
+    i[1] += 1;
+  }
 }
 
 void	print_int_opt(t_flags *flags, int c, int size, int *i)
 {
-	i[1] += flags->min_size - size;
-	if (flags->optmin)
-	{
-		print_plus(flags, i, c);
-		if (flags->prec)
-			ft_filler('0', flags->prec - ft_nbrlen(c));
-		ft_putnbr(c);
-		fill_it(flags, flags->min_size - size);
-	}
-	else
-	{
-		fill_it(flags, flags->min_size - size);
-		print_plus(flags, i, c);
-		if (flags->prec)
-			ft_filler('0', flags->prec - ft_nbrlen(c));
-		ft_putnbr(c);
-	}
+  i[1] += flags->min_size - size;
+  if (flags->optmin)
+  {
+    print_plus(flags, i, c);
+    if (flags->optzero || flags->prec)
+      c < 0 ? (c = -c, ft_putchar('-')) : (c += 0);
+    if (flags->prec)
+      ft_filler('0', flags->prec - ft_nbrlen(c));
+    ft_putnbr(c);
+    if (flags->min_size)
+      fill_it(flags, flags->min_size - size);
+  }
+  else
+  {
+    print_plus(flags, i, c);
+    if (flags->optzero || flags->prec)
+      c < 0 ? (c = -c, ft_putchar('-')) : (c += 0);
+    if (flags->min_size)
+      fill_it(flags, flags->min_size - size);
+    if (flags->prec)
+      ft_filler('0', flags->prec - ft_nbrlen(c));
+    ft_putnbr(c);
+  }
 }
 
 void  print_no_min(t_flags *flags, int *i, int c)
 {
   print_plus(flags, i, c);
+  c < 0 ? (c = -c, ft_putchar('-')) : (c += 0);
   if (flags->prec)
     ft_filler('0', flags->prec - ft_nbrlen(c));
   ft_putnbr(c);
@@ -60,22 +67,22 @@ void  print_no_min(t_flags *flags, int *i, int c)
 
 int		print_int_fill(t_flags *flags, int *i, int c)
 {
-	int size;
+  int size;
 
-	size = ft_nbrlen(c);
-	if (flags->prec)
-		size += (flags->prec - size);
-	if ((flags->optplus && c > 0) || c < 0)
-		size += 1;
-	if (flags->min_size && size < flags->min_size)
-		print_int_opt(flags, c, size, i);
+  size = ft_nbrlen(c);
+  if (flags->prec)
+    size += (flags->prec - size);
+  if ((flags->optplus && c > 0) || c < 0)
+    size += 1;
+  if (flags->min_size && size < flags->min_size)
+    print_int_opt(flags, c, size, i);
   else
     print_no_min(flags, i, c);
-	if ((flags->optplus && c > 0) || c < 0)
-		size -= 1;
-	if (c < 0)
-		size += 1;
-	return (size);
+  if ((flags->optplus && c > 0) || c < 0)
+    size -= 1;
+  if (c < 0)
+    size += 1;
+  return (size);
 }
 
 void  cast_int(int *c, va_list list, t_flags *flags)
@@ -90,33 +97,33 @@ void  cast_int(int *c, va_list list, t_flags *flags)
 
 void	print_int(t_flags *flags, va_list list, int *i)
 {
-	int c;
-	int size;
+  int c;
+  int size;
 
   cast_int(&c, list, flags);
-	if (flags->prec || flags->min_size)
-		size = print_int_fill(flags, i, c);
-	else
-	{
-		size = ft_nbrlen(c);
-		if (c < 0)
-			size += 1;
-		if (c > 0 && flags->optplus)
-		{
-			ft_putchar('+');
-			size += 1;
-		}
-		ft_putnbr(c);
-	}
-	i[1] += size;
+  if (flags->prec || flags->min_size)
+    size = print_int_fill(flags, i, c);
+  else
+  {
+    size = ft_nbrlen(c);
+    if (c < 0)
+      size += 1;
+    if (c > 0 && flags->optplus)
+    {
+      ft_putchar('+');
+      size += 1;
+    }
+    ft_putnbr(c);
+  }
+  i[1] += size;
 }
 
 void	arg_is_int(t_flags *flags, va_list list, int *i)
 {
 
-	if (((flags->type == 'd' || flags->type == 'i') && flags->formf == 'l') ||\
-  	flags->type == 'D' || (flags->formf == 'j') || (flags->formf == 'z'))
-		arg_is_longi(flags, list, i);
-	else
+  if ((flags->formf == 'l') ||\
+      flags->type == 'D' || (flags->formf == 'j') || (flags->formf == 'z'))
+    arg_is_longi(flags, list, i);
+  else
     print_int(flags, list, i);
 }
